fcee57318b9ac7f2cf8916db35bccc05188545c1
Schuyler Wang <schuyler.rw@gmail.com>
2024-04-12 00:58:51 -0700

    model evaluation, TODO: Test additional model parameters

--- validation.py
+++ validation.py
@@ -3,14 +3,14 @@ import numpy as np
 import os
 import sys
 import tensorflow as tf
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import train_test_split
 from main import load_data
 
 EPOCHS = 10
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
 NUM_CATEGORIES = 43
-TEST_SIZE = 0.7
+TEST_SIZE = 0.5
 
 
 def main():
